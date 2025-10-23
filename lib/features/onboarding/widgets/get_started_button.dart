525ef9aff584a051d49ffd3060f563037094b956
Asem Qadi <asem5141@gmail.com>
2025-10-23 23:32:14 +0300

    added Onboarding SCreen & app logo

---
+++ lib/features/onboarding/widgets/get_started_button.dart
@@ -0,0 +1,27 @@
+import 'package:adv_app/core/helpers/extensions.dart';
+import 'package:adv_app/core/reouting/routes.dart';
+import 'package:adv_app/core/theming/app_colors.dart';
+import 'package:adv_app/core/theming/text_styles.dart';
+import 'package:flutter/material.dart';
+
+class GetStartedButton extends StatelessWidget {
+  const GetStartedButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(AppColors.mainBlue),
+        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        minimumSize: MaterialStateProperty.all(const Size(double.infinity, 52)),
+        shape: MaterialStateProperty.all(
+          RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+        ),
+      ),
+      child: Text('Get Started', style: TextStyles.font16WhiteSemoBold),
+      onPressed: () {
+        context.pushNamed(Routes.loginScreen);
+      },
+    );
+  }
+}
