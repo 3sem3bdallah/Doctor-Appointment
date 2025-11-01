9cd5774c0f65c2f613232ad84c9ad3d8ead2045e
Asem Qadi <asem5141@gmail.com>
2025-11-01 23:36:35 +0200

    Add Flavor

--- lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:adv_app/core/di/dependency_injection.dart';
-import 'package:adv_app/core/reouting/app_router.dart';
-import 'package:adv_app/doc_app.dart';
-import 'package:flutter/material.dart';
-
-void main() {
-  setUpGetIt();
-  runApp(DocApp(appRouter: AppRouter()));
-}
